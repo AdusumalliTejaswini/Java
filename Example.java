061106b87fa510342ea3688f19ed50c66843c278
AdusumalliTejaswini <114715860+AdusumalliTejaswini@users.noreply.github.com>
2022-10-10 22:39:53 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example {
+	public static void main(String args[]) {
+		int num = 100;
+		System.out.println("This is num:"+100);
+		num = num*2;
+		System.out.println("The value of num*2 is:");
+		System.out.println(num);
+	}
+
+}
