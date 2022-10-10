99a1b6e4e3739fd7ecb62ac4ba0869c3f423093b
AdusumalliTejaswini <114715860+AdusumalliTejaswini@users.noreply.github.com>
2022-10-10 22:33:00 +0530

    Create BoxDemo.java

---
+++ BoxDemo.java
@@ -0,0 +1,16 @@
+public class BoxDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box mybox=new Box();
+		double vol;
+		mybox.width=10;
+		mybox.depth=20;
+		mybox.height=30;
+		vol=mybox.width*mybox.depth*mybox.height;
+		System.out.println("Volume is:"+vol);
+		
+
+	}
+
+}
